eea15cfbabc1daa6ddbbbbf8325fabd5efd1b2a4
Nisaja26 <nissaikaracmawati@gmail.com>
2024-10-25 00:18:50 +0700

    eps 07 full

---
+++ app/Http/Controllers/AdminUserController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AdminUserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        $data = [
+            'user' => User::get(),
+            'content' => 'admin.user.index'
+        ];
+        return view('admin.layouts.wrapper', $data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+        $data = [
+            'content' => 'admin.user.create'
+        ];
+        return view('admin.layouts.wrapper', $data);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+        $data = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required',
+            're_password' => 'required|same:password',
+
+        ]);
+
+        User::create($data);
+        return redirect('/admin/user');
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
